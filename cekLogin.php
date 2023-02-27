75538e0c1b4848091c39815a66096717645b0162
rayaadhary <adharyr9@gmail.com>
2023-02-27 11:46:35 +0700

    kasir

---
+++ cekLogin.php
@@ -0,0 +1,41 @@
+<?php
+
+include_once "functions.php";
+session_start();
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+// $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+$data = getPenggunaById($username);
+
+if (isset($_POST["btn_login"])) {
+  // if (password_verify($data['password'], $password)) {
+  if ($password == $data['password']) {
+    $_SESSION['role'] = 'pemilik' || 'karyawan';
+    $_SESSION['username'] = $data['username'];
+    $_SESSION['nama'] = $data['nama'];
+    $_SESSION['role'] = $data['role'];
+    setFlash("berhasil", "login", "success");
+    header('Location: ' . BASEURL . '/dashboard.php');
+    //jika rememberme di klik
+    if (!empty($_POST["remember"])) {
+      //buat cookie
+      setcookie("username", $_POST["username"], time() + (3600 * 365 * 24 * 60 * 60));
+      setcookie("password", $_POST["password"], time() + (3600 * 365 * 24 * 60 * 60));
+    } else {
+      if (isset($_COOKIE["username"])) {
+        setcookie("username", "");
+      }
+      if (isset($_COOKIE["password"])) {
+        setcookie("password", "");
+      }
+    }
+    // }
+  } else {
+    setFlash("gagal", "login", "danger");
+    header('location:index.php');
+  }
+} else {
+  header('location:index.php?msg=2');
+}
---
+++ functions.php
@@ -0,0 +1,77 @@
+<?php
+
+define('BASEURL', 'http://localhost/kasir/admin');
+
+function dbConnect()
+{
+  $db = mysqli_connect('localhost', 'root', '', 'kasir');
+  return $db;
+}
+
+function logout()
+{
+  session_start();
+  session_destroy();
+  header("Location: index.php");
+}
+
+
+function getAllPelanggan()
+{
+  $db = dbConnect();
+  $res = mysqli_query($db, "SELECT * FROM pelanggan");
+  $data = $res->fetch_all(MYSQLI_ASSOC);
+  $res->free();
+  return $data;
+}
+
+function getPelangganById($id)
+{
+  $db = dbConnect();
+  $res = mysqli_query($db, "SELECT * FROM pelanggan WHERE id = '$id'");
+  $data = $res->fetch_assoc();
+  $res->free();
+  return $data;
+}
+
+function getPenggunaById($id)
+{
+  $db = dbConnect();
+  $res = mysqli_query($db, "SELECT * FROM pengguna WHERE username = '$id'");
+  $data = $res->fetch_assoc();
+  $res->free();
+  return $data;
+}
+
+function bisa($db, $query)
+{
+  $db = mysqli_query($db, $query);
+
+  if ($db) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+function setFlash($pesan, $aksi, $tipe)
+{
+  $_SESSION['flash'] = [
+    'pesan' => $pesan,
+    'aksi' => $aksi,
+    'tipe' => $tipe
+  ];
+}
+
+function flash()
+{
+  if (isset($_SESSION['flash'])) {
+    echo '<div class="alert alert-' . $_SESSION['flash']['tipe'] . ' alert-dismissible fade show" role="alert">
+      Data <strong>' . $_SESSION['flash']['pesan'] . '</strong> ' . $_SESSION['flash']['aksi'] . '
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      </div>';
+    unset($_SESSION['flash']);
+  }
+}
