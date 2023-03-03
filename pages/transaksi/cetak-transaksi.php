e174e9530de19c8ce4e1d6503050aeb33954c6f3
rayaadhary <adharyr9@gmail.com>
2023-03-03 19:26:20 +0700

    subtotal

--- pages/transaksi/cetak-transaksi.php
+++ pages/transaksi/cetak-transaksi.php
@@ -38,6 +38,8 @@ $pdf->Cell(50, 5, 'Harga Satuan', 1, 0, 'C');
 $pdf->Cell(60, 5, 'Total', 1, 1, 'C'); // Pindah ke baris baru
 $pdf->SetFont('Arial', '', 11);
 $no =  $_SESSION['cetak']['no'] - 1;
+var_dump($_SESSION['cetak']);
+die;
 for ($i = 0; $i < $no; $i++) {
   $pdf->Cell(15, 5, '' . $i + 1, 1, 0, 'C');
   $pdf->Cell(100, 5, '' . $_SESSION['cetak']['nama_barang'][$i], 1, 0);
@@ -46,7 +48,46 @@ for ($i = 0; $i < $no; $i++) {
   $pdf->Cell(50, 5, 'Rp. ' . number_format($_SESSION['cetak']['harga'][$i], 2, ',', '.'), 1, 0, 'R');
   $pdf->Cell(60, 5, 'Rp. ' . number_format($_SESSION['cetak']['subtotal'][$i], 2, ',', '.'), 1, 1, 'R'); // Pindah ke baris baru
 }
-// Isi data faktur/invoice di sini
-
-
+$pdf->SetFont('Arial', 'B', 11);
+$pdf->Cell(20, 5, '', 0, 0, 'C');
+$pdf->Cell(60, 5, 'Tanda Terima', 0, 0, 'C');
+$pdf->Cell(60, 5, 'Hormat Kami', 0, 0, 'C');
+$pdf->Cell(25, 5, '', 0, 0, 'C');
+$pdf->Cell(50, 5, 'Subtotal', 0, 0);
+$pdf->SetFont('Arial', '', 11);
+$pdf->Cell(60, 5, 'Rp.' . number_format($_SESSION['cetak']['total'], 2, ',', '.'), 0, 1, 'R');
+$pdf->SetFont('Arial', '', 11);
+$pdf->Cell(20, 5, '', 0, 0, 'C');
+$pdf->Cell(60, 5, '', 0, 0, 'C');
+$pdf->Cell(60, 5, '', 0, 0, 'C');
+$pdf->Cell(25, 5, '', 0, 0, 'C');
+$pdf->SetFont('Arial', 'B', 11);
+$pdf->Cell(50, 5, 'Potongan', 0, 0);
+$pdf->Cell(60, 5, '', 0, 1, 'R');
+$pdf->SetFont('Arial', '', 11);
+$pdf->Cell(20, 5, '', 0, 0, 'C');
+$pdf->Cell(60, 5, '', 0, 0);
+$pdf->Cell(60, 5, '', 0, 0, 'C');
+$pdf->Cell(25, 5, '', 0, 0, 'C');
+$pdf->SetFont('Arial', 'B', 11);
+$pdf->Cell(50, 5, 'Total', 0, 0);
+$pdf->Cell(60, 5, '', 0, 1, 'R');
+$pdf->SetFont('Arial', '', 11);
+$pdf->Cell(20, 5, '', 0, 0, 'C');
+$pdf->Cell(60, 5, '', 0, 0);
+$pdf->Cell(60, 5, '', 0, 0, 'C');
+$pdf->Cell(25, 5, '', 0, 0, 'C');
+$pdf->SetFont('Arial', 'B', 11);
+$pdf->Cell(50, 5, 'Bayar', 0, 0);
+$pdf->Cell(60, 5, '', 0, 1, 'R');
+$pdf->SetFont('Arial', '', 11);
+$pdf->Cell(20, 5, '', 0, 0, 'C');
+$pdf->SetFont('Arial', 'B', 11);
+$pdf->Cell(60, 5, '(   Putra Subur Makmur   )', 0, 0, 'C');
+$pdf->SetFont('Arial', 'B', 10);
+$pdf->Cell(60, 5, '(   PT AJITA PANELWIRA PERKASA   )', 0, 0, 'C');
+$pdf->Cell(25, 5, '', 0, 0, 'C');
+$pdf->SetFont('Arial', 'B', 11);
+$pdf->Cell(50, 5, 'Sisa', 0, 0);
+$pdf->Cell(60, 5, '', 0, 1, 'R');
 $pdf->Output();
