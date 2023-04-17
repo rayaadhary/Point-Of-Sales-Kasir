bb0c5c3b4abf4921058decd3d9ec287899c2d318
rayaadhary <adharyr9@gmail.com>
2023-04-17 17:01:54 +0700

    logic jquery form transaksi utang

---
+++ pages/barang-masuk/ambilDataBarangMasuk.php
@@ -0,0 +1,10 @@
+ 
+ <?php
+  include_once '../../functions.php';
+  $db = dbConnect();
+  if (isset($_POST["no_barang_masuk"])) {
+    $no_barang_masuk = mysqli_real_escape_string($db, trim($_POST['no_barang_masuk']));
+    $query = $db->query("SELECT * FROM barang_masuk WHERE no_barang_masuk = '" . $no_barang_masuk . "'");
+    $data = $query->fetch_array();
+    echo json_encode($data);
+  }
---
+++ pages/barang-masuk/barang-masuk-pelunasan.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once "../../functions.php";
+
+$db = dbConnect();
+
+if (isset($_POST["update_utang"])) {
+  $no_faktur = mysqli_real_escape_string($db, trim($_POST["no_faktur"]));
+  $diskon = mysqli_real_escape_string($db, trim($_POST["diskon_baru"]));
+  $bayar = mysqli_real_escape_string($db, trim($_POST["bayar_baru"]));
+  $kembalian = mysqli_real_escape_string($db, trim($_POST["kembalian"]));
+  $status = mysqli_real_escape_string($db, trim($_POST["status"]));
+  $res = $db->prepare("UPDATE transaksi SET kembali=?, bayar=?, status=? WHERE no_faktur=?");
+  $res->bind_param("sssss", $diskon,  $kembalian, $bayar, $status, $no_faktur);
+  $res->execute();
+  if ($res) {
+    setFlash('berhasil', 'diubah', 'primary');
+    header('Location: ' . BASEURL . '/pages/transaksi/transaksi-utang.php');
+    exit;
+  } else {
+    setFlash('gagal', 'diubah', 'danger');
+    header('Location: ' . BASEURL . '/pages/transaksi/transaksi-utang.php');
+    exit;
+  }
+  $db->close();
+}
